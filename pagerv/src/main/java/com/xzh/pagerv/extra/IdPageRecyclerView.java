0718e01486a789d8b0776b0d18b731bb2f9c643c
xiezihao <766770719@qq.com>
2016-12-05 11:46:56 +0800

    初次提交

---
+++ pagerv/src/main/java/com/xzh/pagerv/extra/IdPageRecyclerView.java
@@ -0,0 +1,38 @@
+package com.xzh.pagerv.extra;
+
+import android.content.Context;
+
+import com.xzh.pagerv.core.BaseRecyclerView;
+
+import java.util.List;
+
+/**
+ * 通过数据ID分页的RecyclerView
+ * Created by xiezihao on 16/12/5.
+ */
+public class IdPageRecyclerView extends BaseRecyclerView {
+
+    public IdPageRecyclerView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void refresh(Object key) {
+
+    }
+
+    @Override
+    public void loadData(Object key) {
+
+    }
+
+    @Override
+    public void onLoadSuccess(Object key, List data) {
+
+    }
+
+    @Override
+    public void onLoadFailed(Object key) {
+
+    }
+}
